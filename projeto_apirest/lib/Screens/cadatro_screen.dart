18c55f261a3b244c90696c37da72cc0ffe69c0f8
Ezequielzz <ezequielsamueldl@gmail.com>
2024-06-03 16:57:55 -0300

    Método Vazio

--- projeto_apirest/lib/Screens/cadatro_screen.dart
+++ projeto_apirest/lib/Screens/cadatro_screen.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_apirest/Controller/controller.dart';
 
-class CadatroScreen extends StatelessWidget {
+class CadatroScreen extends StatefulWidget {
   const CadatroScreen({super.key});
+
+  @override
+  State<CadatroScreen> createState() => _CadatroScreenState();
+}
+
+class _CadatroScreenState extends State<CadatroScreen> {
+  ProdutoController _controller = ProdutoController();
+  TextEditingController _idController = TextEditingController();
+  TextEditingController _nomeController = TextEditingController();
+  TextEditingController _precoController = TextEditingController();
+
+  Future<void> _cadastrarProduto() async {
+    
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+
     );
   }
 }
