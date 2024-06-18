80c61729838aa5d7d9c9d4f6a890303cbd8ef772
Ezequielzz <123770595+Ezequielzz@users.noreply.github.com>
2024-06-18 10:31:58 -0300

    Update search_screen.dart

--- projeto_api_clima_loc/lib/Screens/search_screen.dart
+++ projeto_api_clima_loc/lib/Screens/search_screen.dart
@@ -5,109 +5,134 @@ import 'package:projeto_api_clima_loc/Model/cidade_db_model.dart';
 import 'package:projeto_api_clima_loc/Screens/city_detail_screen.dart';
 
 class SearchScreen extends StatefulWidget {
-  const SearchScreen({super.key});
+  const SearchScreen({super.key}); // Construtor da tela de busca
 
   @override
   State<SearchScreen> createState() => _SearchScreenState();
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  TextEditingController _cityController = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
-  final WeatherController _controller = WeatherController();
-  final CidadeDbController _cidadeDbController = CidadeDbController();
+  TextEditingController _cityController =
+      TextEditingController(); // Controlador para o campo de texto da cidade
+  final _formKey = GlobalKey<FormState>(); // Chave global para o formulário
+  final WeatherController _controller =
+      WeatherController(); // Instância do controlador de clima
+  final CidadeDbController _cidadeDbController =
+      CidadeDbController(); // Instância do controlador de cidade do banco de dados
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Buscar Cidade',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
+      appBar: AppBar(
+        title: const Text(
+          'Buscar Cidade',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Color.fromARGB(
+            255, 0, 225, 255), // Cor de fundo da barra de aplicativo
+        foregroundColor: Color.fromARGB(
+            255, 255, 255, 255), // Cor do texto da barra de aplicativo
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Form(
+            key: _formKey, // Associa a chave global ao formulário
+            child: Column(
+              children: [
+                TextFormField(
+                  controller:
+                      _cityController, // Controlador para o campo de texto da cidade
+                  decoration: const InputDecoration(
+                    labelText: 'Insira a cidade',
+                  ),
+                  validator: (value) {
+                    if (value!.trim().isEmpty) {
+                      return 'Por favor, insira a cidade'; // Validação do campo de texto da cidade
+                    }
+                    return null;
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _buscarCidade(_cityController
+                          .text); // Chama o método para buscar a cidade ao pressionar o botão
+                    }
+                  },
+                  child: const Text('Search'), // Texto do botão de busca
+                ),
+                const SizedBox(height: 20),
+                Expanded(
+                  child: FutureBuilder(
+                    future: _cidadeDbController
+                        .getAllCidades(), // Obtém todas as cidades favoritas do banco de dados
+                    builder: (context, snapshot) {
+                      if (_cidadeDbController.getCidades().isEmpty) {
+                        return const Center(
+                            child:
+                                CircularProgressIndicator()); // Indicador de progresso enquanto carrega as cidades favoritas
+                      } else {
+                        return ListView.builder(
+                          itemCount: _cidadeDbController.getCidades().length,
+                          itemBuilder: (context, index) {
+                            final reversedIndex =
+                                _cidadeDbController.getCidades().length -
+                                    index -
+                                    1;
+                            return ListTile(
+                              title: Text(_cidadeDbController
+                                  .getCidades()[reversedIndex]
+                                  .nomeCidade), // Exibe o nome da cidade favorita
+                              onTap: () {
+                                _buscarCidade(_cidadeDbController
+                                    .getCidades()[reversedIndex]
+                                    .nomeCidade); // Busca a cidade ao clicar na lista de cidades favoritas
+                              },
+                            );
+                          },
+                        );
+                      }
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
-          backgroundColor: Color.fromARGB(255, 0, 225, 255),
-          foregroundColor: Color.fromARGB(255, 255, 255, 255),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Center(
-              child: Form(
-                  key: _formKey,
-                  child: Column(children: [
-                    TextFormField(
-                      controller: _cityController,
-                      decoration: const InputDecoration(
-                        labelText: 'Insira a cidade',
-                      ),
-                      validator: (value) {
-                        if (value!.trim().isEmpty) {
-                          return 'Por favor, insira a cidade';
-                        }
-                        return null;
-                      },
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          _buscarCidade(_cityController.text);
-                        }
-                      },
-                      child: const Text('Search'),
-                    ),
-                    const SizedBox(height: 20),
-                    Expanded(
-                        child: FutureBuilder(
-                            future: _cidadeDbController.getAllCidades(),
-                            builder: (context, snapshot) {
-                              if (_cidadeDbController.getCidades().isEmpty) {
-                                return const Center(
-                                    child: CircularProgressIndicator());
-                              } else {
-                                return ListView.builder(
-                                    itemCount:
-                                        _cidadeDbController.getCidades().length,
-                                    itemBuilder: (context, index) {
-                                      final reversedIndex = _cidadeDbController.getCidades().length - index - 1;
-                                      return ListTile(
-                                        title: Text(_cidadeDbController
-                                            .getCidades()[reversedIndex]
-                                            .nomeCidade),
-                                        onTap: () {
-                                          _buscarCidade(_cidadeDbController
-                                              .getCidades()[reversedIndex]
-                                              .nomeCidade);
-                                        },
-                                      );
-                                    });
-                              }
-                            }))
-                  ]))),
-        ));
+      ),
+    );
   }
 
   Future<void> _buscarCidade(String city) async {
     if (await _controller.buscarCidade(city)) {
-      CidadeDb db = CidadeDb(nomeCidade: city, favorito: 0);
-      _cidadeDbController.create(db); // objeto da classe city db
-      // Snackbar
+      CidadeDb db = CidadeDb(
+          nomeCidade: city,
+          favorito:
+              0); // Cria um objeto CidadeDb com o nome da cidade e define como não favorita
+      _cidadeDbController.create(db); // Salva a cidade no banco de dados
+      // Snackbar para exibir mensagem de sucesso
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
           content: Text('Cidade encontrada!'), duration: Duration(seconds: 2)));
-      // Redirecionando para a página
+      // Redireciona para a página de detalhes da cidade encontrada
       Navigator.push(
           context,
           MaterialPageRoute(
               builder: (context) => CityDetailsScreen(city: city)));
-              setState(() {
-                _cidadeDbController.getCidades().clear();
-              });
+      setState(() {
+        _cidadeDbController
+            .getCidades()
+            .clear(); // Limpa a lista de cidades após redirecionar para a tela de detalhes
+      });
     } else {
-      _cityController.clear();
-      // Snackbar
+      _cityController.clear(); // Limpa o campo de texto da cidade
+      // Snackbar para exibir mensagem de cidade inválida
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
           content: Text('Cidade inválida!'), duration: Duration(seconds: 2)));
-      setState(() {});
+      setState(
+          () {}); // Atualiza o estado para refletir a mensagem de cidade inválida
     }
   }
 }
